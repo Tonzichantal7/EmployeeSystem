8c55306acfc25d8c931f7b2aa174222fb2581ae0
chantal <chantaluwitonze6@gmail.com>
2025-11-20 11:03:10 +0200

    Add Docs and Swagger setup

--- src/main/java/com/example/employeemanagement/config/SwaggerConfig.java
+++ src/main/java/com/example/employeemanagement/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.example.employeemanagement.config;
 
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +12,22 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Employee Management API")
                         .version("1.0")
-                        .description("API documentation for Employee Management System"));
+                        .description("API documentation for Employee Management System"))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(
+                        new io.swagger.v3.oas.models.Components()
+                                .addSecuritySchemes("BearerAuth",
+                                        new SecurityScheme()
+                                                .name("Authorization")
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
     }
 }
