4f276d2b3238e3ce41acea7db49072902e7a2e4b
chantal <chantaluwitonze6@gmail.com>
2025-11-20 09:48:37 +0200

    add user registration and login endpoints with JWT authentication and role support

--- src/main/java/com/example/employeemanagement/controller/AuthController.java
+++ src/main/java/com/example/employeemanagement/controller/AuthController.java
@@ -4,11 +4,13 @@ import com.example.employeemanagement.dto.AuthRequest;
 import com.example.employeemanagement.dto.AuthResponse;
 import com.example.employeemanagement.entities.User;
 import com.example.employeemanagement.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "AuthController", description = "User Registration & Login")
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
